418b0734afd602e159b847e81351ff1cf485c05c
Teodor Vasić <37002784+VasicTeodor@users.noreply.github.com>
2021-03-25 23:42:59 +0100

    Initial commit.

---
+++ DocumentManager/DocumentManager.Domain/Models/Book.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DocumentManager.Domain.Models
+{
+    public class Book
+    {
+        public string Title { get; set; }
+        public string Writers { get; set; }
+        public string Genre { get; set; }
+        public long Isbn { get; set; }
+        public string KeyWords { get; set; }
+        public string Publisher { get; set; }
+        public DateTime Year { get; set; }
+        public string Place { get; set; }
+        public string Synopsis { get; set; }
+        public int PageNumber { get; set; }
+    }
+}
